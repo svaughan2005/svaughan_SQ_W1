736c6296952c59cc9a3f6ca15eb933df8775840c
svaughan2005 <svaughan@uwaterloo.ca>
2026-05-13 18:08:09 -0400

    everything is in i think
    i think i made all the correct changes to the file - maybe needa change the read me file tho

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
+let bgImg;
+let towerImg;
+
+function preload() {
+  bgImg = loadImage("assets/images/table-bg.jpg"); // load the background img before the sketch starts
+  towerImg = loadImage("assets/images/jenga-tower.png"); // load jenga tower img
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 300);
 }
 
 function draw() {
   background(220, 100, 10);
+  image(bgImg, 0, 0);
+  image(towerImg, 25, 0);
+
+  rect(50, 130, 100, 20);
+  text("This is jenga!", 60, 145);
 }
